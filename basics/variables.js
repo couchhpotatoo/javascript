b02e78ed64c952264c26ad149d0fc3699b79c023
Pooja Rawat <130988642+poojaa09@users.noreply.github.com>
2025-01-01 19:13:53 +0000

    commit done

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+ const id=3482949
+ let email="abc@gmail.com"
+ var password="570357"
+ city="pauri"
+ //id=7698 //not allowed
+ email="hello@hello.com"
+ password="3743742498"
+ city="chamoli"
+
+ console.log(id)
+ console.table([id,email,password,city])
+ /*
+ prefer not to use var 
+ because of issue in block and functional sccope*/
