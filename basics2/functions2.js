3d6c5d022751ce2c83078b4271c9b7121f3ec61e
PR <130988642+couchhpotatoo@users.noreply.github.com>
2025-02-01 19:25:18 +0000

    done

---
+++ basics2/functions2.js
@@ -0,0 +1,21 @@
+function calculatePrice(...num1){
+    return num1
+}
+console.log(calculatePrice(434,434,656,412,324))
+const user={
+    username:"pooja",
+    city:"pauri"
+}
+function handleObject(anyobject){
+    console.log(`username : ${anyobject.username} and city is ${anyobject.city}`);
+    
+}
+console.log(handleObject(user));
+
+// array
+const arr=[7932,3298,682,4628623]
+function handleArray(anyarray){
+    return anyarray[3]
+    
+}
+console.log(handleArray(arr))
