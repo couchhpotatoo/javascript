ec259434ba818bc49cf55afb27efee725fb3b8c8
PR <130988642+couchhpotatoo@users.noreply.github.com>
2025-02-03 16:28:07 +0000

    done

---
+++ iterations/for.js
@@ -0,0 +1,31 @@
+// for (let index = 0; index < 10; index++) {
+//     const element =index;
+//     if(element==5){
+//         console.log("pause here");
+        
+//     }
+//     console.log(element);
+    
+// }
+// for (let index = 0; index <=10; index++) {
+//     console.log(`outer loop ${index}`);
+    
+//    for (let i = 0; i < 5; i++) {
+//     console.log(`inner value ${i}`);
+    
+    
+//    }
+    
+//}
+for (let i = 1; i <= 5; i++) {
+   for (let j = 1; j <= 10; j++) {
+    console.log(i + "x" + j + "=" + i*j)
+    
+   }
+    
+}
+const array=[68,58,747,6698]
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    
+}
