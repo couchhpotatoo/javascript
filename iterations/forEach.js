c45af924973b236c874063ead34f5e1dc353c7f5
PR <130988642+couchhpotatoo@users.noreply.github.com>
2025-02-05 02:36:15 +0000

    done

--- iterations/forEach.js
+++ iterations/forEach.js
@@ -12,22 +12,43 @@ const programming=["js","py","cpp"]
 // programming.forEach((item,index,array)=>{
 //     console.log(item,index,array)
 // })
-const code=[{
-    name:"javascript",
-    file:"js"
-},
-{
-    name:"java",
-    file:"java"
-},
-{
-    name:"c++",
-    file:"cpp"
-}
-]
-code.forEach(
-    (item)=>{
-        console.log(item.file)
+// const code=[{
+//     name:"javascript",
+//     file:"js"
+// },
+// {
+//     name:"java",
+//     file:"java"
+// },
+// {
+//     name:"c++",
+//     file:"cpp"
+// }
+// ]
+// code.forEach(
+//     (item)=>{
+//         console.log(item.file)
+//     }
+// )
+// const values= programming.forEach((item)=>{
+// console.log(item);
+// return item
+// })
+// console.log(values)  //returns undefined
+const nums=[1,2,3,4,5,6,7,8,9,10]
+// let numb=nums.filter((num) => num>4)
+// console.log(numb);
+const newNum=[]
+nums.forEach((num)=>{
+    if(num>4){
+        newNum.push(num)
     }
-)
+
+})
+console.log(newNum);
+
+
+
+
+    
 
